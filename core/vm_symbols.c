8c8979aa1d5aefa8c9fbdc59aa2e635406a1fdaa
Luna <borsuczek@gmail.com>
2018-06-05 23:18:40 +0200

    Fix issue with line number symbols

--- core/vm_symbols.c
+++ core/vm_symbols.c
@@ -7,12 +7,12 @@ vm_symbols_t vm_symbols;
 
 static inline uint32_t upper_bound(vm_symbols_entry_t* data, uint32_t size, uint32_t key) {
 	uint32_t lo = 0;
-	uint32_t hi = size-1;
+	uint32_t hi = size - 1;
 	while (lo < hi) {
 		uint32_t mid = lo + ((hi - lo) >> 1);
 		if (data[mid].key > key) {
 			hi = mid;
-		}else if (data[mid].key < key) {
+		} else if (data[mid].key < key) {
 			lo = mid + 1;
 		} else {
 			return mid;
@@ -23,36 +23,34 @@ static inline uint32_t upper_bound(vm_symbols_entry_t* data, uint32_t size, uint
 
 static inline uint32_t lower_bound(vm_symbols_entry_t* data, uint32_t size, uint32_t key) {
 	uint32_t lo = 0;
-	uint32_t hi = size-1;
+	uint32_t hi = size - 1;
 	while (lo < hi) {
 		uint32_t mid = lo + ((hi - lo) >> 1);
 		if (data[mid].key > key) {
 			hi = mid;
-		}else if (data[mid].key < key) {
+		} else if (data[mid].key < key) {
 			lo = mid + 1;
 		} else {
 			return mid;
 		}
 	}
-	return (data[lo].key > key && lo > 0) ? lo-1 : lo;
+	return (data[lo].key > key && lo > 0) ? lo - 1 : lo;
 }
 
 uint32_t vm_symbols_get_line(uint32_t offset) {
-	uint32_t index = upper_bound(vm_symbols.lines,vm_symbols.lines_count,offset);
-	if (vm_symbols.lines[0].key > offset)
-		return 0;
+	uint32_t index = upper_bound(vm_symbols.lines, vm_symbols.lines_count, offset);
 	return vm_symbols.lines[index].value;
 }
 
 vm_mmid_t vm_symbols_get_function(uint32_t offset) {
-	uint32_t index = lower_bound(vm_symbols.functions,vm_symbols.functions_count,offset);
+	uint32_t index = lower_bound(vm_symbols.functions, vm_symbols.functions_count, offset);
 	if (vm_symbols.functions[0].key > offset)
 		return MMID_NULL;
 	return vm_symbols.functions[index].value;
 }
 
 wstring_t* vm_symbols_get_file(uint32_t offset) {
-	uint32_t index = lower_bound(vm_symbols.files,vm_symbols.files_count,offset);
+	uint32_t index = lower_bound(vm_symbols.files, vm_symbols.files_count, offset);
 	if (vm_symbols.files[0].key > offset)
 		return NULL;
 	return vm_symbols.strings[vm_symbols.files[index].value];
@@ -71,7 +69,7 @@ void vm_symbols_get_location(uint32_t pc, vm_symbols_location_t* loc) {
 		vm_mmid_t func = vm_symbols_get_function(pc);
 		if (func != MMID_NULL) {
 			vm_mmid_t name = MMID_TO_PTR(func, vm_hashmap_t*)->name;
-			loc->function = (wstring_t*)(MMID_TO_PTR(name, uint8_t*)+4);
+			loc->function = (wstring_t*)(MMID_TO_PTR(name, uint8_t*) + 4);
 		}
 	}
 }
