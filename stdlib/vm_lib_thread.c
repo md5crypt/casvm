e465860d6faf75b504775741c090e69bf6b385d9
Luna <borsuczek@gmail.com>
2019-07-14 12:00:36 +0200

    added thread reattach function

--- stdlib/vm_lib_thread.c
+++ stdlib/vm_lib_thread.c
@@ -31,3 +31,15 @@ vm_exception_t vm_lib_thread_detach(vm_variable_t* top, uint32_t arguments) {
 	top[1] = VM_VARIABLE_OFTYPE(VM_UNDEFINED_T);
 	return VM_NONE_E;
 }
+
+vm_exception_t vm_lib_thread_reattach(vm_variable_t* top, uint32_t arguments) {
+	ASSERT_ARITY(1);
+	ASSERT_TYPE(1, VM_THREAD_T);
+	vm_thread_t* thread = VM_CAST_THREAD(top - 1);
+	if ((thread->flags & (VM_THREAD_FLAG_FINISHED | VM_THREAD_FLAG_DETACHED)) == VM_THREAD_FLAG_DETACHED) {
+		thread->flags &= ~VM_THREAD_FLAG_DETACHED;
+		vm_dereference(thread, VM_THREAD_T);
+	}
+	top[1] = VM_VARIABLE_OFTYPE(VM_UNDEFINED_T);
+	return VM_NONE_E;
+}
